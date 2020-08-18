7151148e126d52eaf0b64684d5fee7f9311d6c3a
YangfeiLiu <gentleman_lyf@163.com>
2020-08-18 21:14:10 +0800

    add file

--- 16.最接近的三数之和.cpp
+++ 16.最接近的三数之和.cpp
@@ -8,7 +8,27 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-
+        int len = nums.size();
+        sort(nums.begin(), nums.end());
+        int diff = INT_MAX;
+        for(int i = 0; i < len - 2; i++){
+            int temp = target - nums[i];
+            int l = i + 1, r = len - 1;
+            while(l < r){
+                int sum = nums[l] + nums[r];
+                if(abs(sum - temp) < abs(diff)){ // 注意处理这里
+                    diff = temp - sum;
+                }
+                if(sum == temp) return target;
+                if(sum > temp) {
+                    r--;
+                }
+                if(sum < temp) {
+                    l++;
+                }
+            }
+        }
+        return target - diff;
     }
 };
 // @lc code=end
